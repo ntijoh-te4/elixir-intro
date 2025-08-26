b0f9f9c52dbacd23db093a392455bee0b9e09b50
Daniel Berg <daniel.berg@ntig.se>
2025-08-26 14:22:38 +0200

    Update Elixir version requirement to 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Intro.MixProject do
     [
       app: :elixir_intro,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
